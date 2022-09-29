58dc964592c1c8fa5c7be1840dce125b350cd309
Mitsuharu Emoto <mthr1982@gmail.com>
2022-09-29 20:15:09 +0900

    Initial Commit

---
+++ ReSwiftSagaSample/ReSwiftSagaSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReSwiftSagaSampleApp.swift
+//  ReSwiftSagaSample
+//
+//  Created by Mitsuharu Emoto on 2022/09/29.
+//
+
+import SwiftUI
+
+@main
+struct ReSwiftSagaSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReSwiftSagaSampleUITests/ReSwiftSagaSampleUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ReSwiftSagaSampleUITestsLaunchTests.swift
+//  ReSwiftSagaSampleUITests
+//
+//  Created by Mitsuharu Emoto on 2022/09/29.
+//
+
+import XCTest
+
+final class ReSwiftSagaSampleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
