45c2cbcb964c49cf180b8fc091132b403b861054
Mitsuharu Emoto <mthr1982@gmail.com>
2022-10-01 19:43:00 +0900

    add CounterView

---
+++ ReSwiftSagaSample/ViewModel/CounterViewModel.swift
@@ -0,0 +1,43 @@
+//
+//  CounterViewModel.swift
+//  ReSwiftSagaSample
+//
+//  Created by Mitsuharu Emoto on 2022/10/01.
+//
+
+import Foundation
+import ReSwift
+
+final class CounterViewModel: ObservableObject, StoreSubscriber {
+    @Published private(set) var count: Int = 0
+
+    init() {
+        appStore.subscribe(self) {
+            $0.select { selectCount(store: $0) }
+        }
+    }
+
+    deinit {
+        appStore.unsubscribe(self)
+    }
+
+    internal func newState(state: Int) {
+        self.count = state
+    }
+    
+    public func increase() {
+        appStore.dispatch(CounterAction.increase)
+    }
+    
+    public func decrease() {
+        appStore.dispatch(CounterAction.decrease)
+    }
+    
+    public func move(count: Int) {
+        appStore.dispatch(CounterAction.move(count: count))
+    }
+
+    public func clear() {
+        appStore.dispatch(CounterAction.clear)
+    }
+}
